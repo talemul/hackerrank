51f57f4ca33274b79b56e85f3cacd5af5057b8a5
Talemul Islam <talemul@users.noreply.github.com>
2022-02-06 02:58:45 +0600

    Update and rename Revising-the-Select-Query.sql I to Revising-the-Select-Query.sql

--- Revising-the-Select-Query.sql
+++ Revising-the-Select-Query.sql
@@ -7,3 +7,5 @@ Select * from CITY where COUNTRYCODE='JPN';
 Select NAME from CITY where COUNTRYCODE='JPN';
 SELECT CITY, STATE FROM STATION;
 SELECT CITY FROM STATION where ID % 2=0 group by CITY;
+
+select * from ((select N,'Inner' from BST where N in (select DISTINCT  P from BST) and p is not null)union(select N,'Root' from BST where P is null)union(select a.N,'Leaf' from BST a left join BST b on a.N=b.p where b.N is null)) d order by d.N ;
