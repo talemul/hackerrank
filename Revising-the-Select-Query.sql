0ebe7f2be3be07ebd60a9221a93912f43a00c4e9
Talemul Islam <talemul@users.noreply.github.com>
2022-02-08 10:28:10 +0600

    Update Revising-the-Select-Query.sql

--- Revising-the-Select-Query.sql
+++ Revising-the-Select-Query.sql
@@ -9,3 +9,6 @@ SELECT CITY, STATE FROM STATION;
 SELECT CITY FROM STATION where ID % 2=0 group by CITY;
 
 select * from ((select N,'Inner' from BST where N in (select DISTINCT  P from BST) and p is not null)union(select N,'Root' from BST where P is null)union(select a.N,'Leaf' from BST a left join BST b on a.N=b.p where b.N is null)) d order by d.N ;
+
+select concat(Name,'(',SUBSTR(OCCUPATION,1,1),')') from OCCUPATIONS order by Name;
+select concat('There are a total of ',count(*),' ',LOWER(occupation),'s.') from OCCUPATIONS group by OCCUPATION order by count(*),OCCUPATION;
