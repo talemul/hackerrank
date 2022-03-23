ce7831c0e44c407a66fd56c95e915939caf398b6
Talemul Islam <talemul@users.noreply.github.com>
2022-03-23 17:47:09 +0600

    Create Arrays-Left-Rotation.js

---
+++ Arrays-Left-Rotation.js
@@ -0,0 +1,64 @@
+'use strict';
+function rotLeft(a, d) {
+    // Write your code here
+    var i,temp,j;
+for(i=0;i<d;i++){
+    temp=a[0];
+    for(j=1;j<a.length;j++){
+        a[j-1]=a[j];
+    }
+    a[a.length-1]=temp;
+   // console.log(a);
+}
+return a;
+}
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
