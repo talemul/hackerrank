560c25f6de715c52be7ad20993f10f7dfd1950e7
Talemul Islam <talemul@users.noreply.github.com>
2022-03-24 00:41:18 +0600

    Create Array-Manipulation.js

---
+++ Array-Manipulation.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const fs = require('fs');
+function arrayManipulation(n, queries) {
+    // Write your code here
+          // problem is known as prefix sum algo.
+    const arr = new Array(n + 2).fill(0), qn = queries.length;
+    let sum = 0, maxValue = 0;
+
+    for (let i = 0; i < qn; i++) {
+        const [min, max, sum] = queries[i];
+       // console.log(min+'max'+max+'sum'+sum);
+        arr[min] += sum;
+        maxValue = maxValue < sum ? sum : maxValue;
+        if (max <= n) {
+            arr[max + 1] -= sum;
+        }
+       // console.log(arr);
+
+    }
+    for (let k = 0; k < n; k++) {
+        sum += arr[k];
+        if (maxValue < sum) {
+            maxValue = sum;
+        }
+       // console.log('sum '+sum);
+    }
+    return maxValue;
+}
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'arrayManipulation' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().replace(/\s+$/g, '').split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    const result = arrayManipulation(n, queries);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
