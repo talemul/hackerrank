eb13c38d513a6912a160d3de9eec0e623eda22c3
Talemul Islam <talemul@users.noreply.github.com>
2022-03-25 11:36:46 +0600

    Create Strings-Making-Anagrams.js

---
+++ Strings-Making-Anagrams.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const fs = require('fs');
+/*
+ * Complete the 'makeAnagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING a
+ *  2. STRING b
+ */
+
+function makeAnagram(a, b) {
+    // Write your code here
+    Array.prototype.repeat= function(what, L){
+    while(L) this[--L]= what;
+    return this;
+    }
+    let i,sum=0,temp=[].repeat(0, 26);    
+for(i=0;i<a.length;i++){
+  temp[a.charCodeAt(i)-"a".charCodeAt(0)]++;
+  console.log(a.charCodeAt(i)-"a".charCodeAt(0));
+}
+console.log(temp);
+for(i=0;i<b.length;i++){
+   temp[b.charCodeAt(i)-"a".charCodeAt(0)]--;  
+}
+ for(i=0;i<temp.length;i++){
+     sum +=Math.abs(temp[i]);
+ } 
+return sum;
+}
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine();
+
+    const b = readLine();
+
+    const res = makeAnagram(a, b);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
+
+/*
+A student is taking a cryptography class and has found anagrams to be very useful. Two strings are anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+The student decides on an encryption scheme that involves two large strings. The encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Determine this number.
+
+Given two strings,  and , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+
+Example
+
+
+Delete  from  and  from  so that the remaining strings are  and  which are anagrams. This takes  character deletions.
+
+Function Description
+
+Complete the makeAnagram function in the editor below.
+
+makeAnagram has the following parameter(s):
+
+string a: a string
+string b: another string
+Returns
+
+int: the minimum total characters that must be deleted
+Input Format
+
+The first line contains a single string, .
+The second line contains a single string, .
+
+Constraints
+
+The strings  and  consist of lowercase English alphabetic letters, ascii[a-z].
+Sample Input
+
+cde
+abc
+Sample Output
+
+4
+Explanation
+
+Delete the following characters from the strings make them anagrams:
+
+Remove d and e from cde to get c.
+Remove a and b from abc to get c.
+It takes  deletions to make both strings anagrams.
+*/
